d32de71d58f31e936571baab74c54f1c6ba929cf
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2021-10-11 17:24:59 +0100

    added working dependences for styling

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+UIkit.use(Icons)
+import './assets/stylesheet/base.less'
+
 Vue.config.productionTip = false
 
 new Vue({
