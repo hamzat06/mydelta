fc583080af6b448bd1cc09d162a8ae1b28b693ce
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2021-10-13 06:36:39 +0100

    Added offcanvas nav for mobile vue and transformed the last line of the footer from flex to grid to make it better responsive

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,28 @@
                         <router-link to="/" class="uk-navbar-item uk-logo uk-text-bold uk-light">MyDelta</router-link>
                       </div>
                       <div class="uk-navbar-right">
-                        <ul class="uk-navbar-nav uk-text-semibold">
-                            <li><router-link to="/" active-class="uk-active">Home</router-link></li>
+                        <a class="uk-navbar-toggle uk-hidden@m" uk-navbar-toggle-icon href="#" uk-toggle="target: #offcanvas-nav-primary"></a>
+                        <ul class="uk-navbar-nav uk-text-semibold uk-visible@m">
+                            <li><router-link to="/">Home</router-link></li>
                             <li><router-link to="/about">About Us</router-link></li>
                             <li><router-link to="/services">Services</router-link></li>
                             <li><router-link to="/clients">Clients</router-link></li>
                             <li><router-link to="/contact">Contact Us</router-link></li>
                         </ul>
+                        <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true; flip: true;" class="uk-hidden@m">
+                          <div class="uk-offcanvas-bar uk-flex uk-flex-column">
+                              <button class="uk-offcanvas-close" type="button" uk-close></button>
+                              
+                              <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                                  <li><router-link to="/" active-class="uk-active">Home</router-link></li>
+                                  <li><router-link to="/about">About Us</router-link></li>
+                                  <li><router-link to="/services">Services</router-link></li>
+                                  <li><router-link to="/clients">Clients</router-link></li>
+                                  <li><router-link to="/contact">Contact Us</router-link></li>
+                              </ul>
+
+                          </div>
+                      </div>
                       </div>
 
                     </div>
@@ -95,14 +110,18 @@
               </div>
             </div>
             <hr>
-            <div class="uk-flex uk-flex-between">
-              <p>My Delta &copy; 2021 / All Rights Reserved</p>
-              <div>
-                <a href="" uk-icon="icon: facebook" class="uk-margin-small-right"></a>
-                <a href="" uk-icon="icon: google" class="uk-margin-small-right"></a>
-                <a href="" uk-icon="icon: instagram" class="uk-margin-small-right"></a>
-                <a href="" uk-icon="icon: twitter" class="uk-margin-small-right"></a>
-                <a href="" uk-icon="icon: youtube" class="uk-margin-small-right"></a>
+            <div>
+              <div class="uk-child-width-1-2@s" uk-grid>
+                <div>
+                  <p>My Delta &copy; 2021 / All Rights Reserved</p>
+                </div>
+                <div class="align-right-small">
+                  <a href="" uk-icon="icon: facebook" class="uk-margin-small-right"></a>
+                  <a href="" uk-icon="icon: google" class="uk-margin-small-right"></a>
+                  <a href="" uk-icon="icon: instagram" class="uk-margin-small-right"></a>
+                  <a href="" uk-icon="icon: twitter" class="uk-margin-small-right"></a>
+                  <a href="" uk-icon="icon: youtube" class="uk-margin-small-right"></a>
+                </div>
               </div>
             </div>
           </div>
@@ -142,6 +161,11 @@ nav {
 
 footer {
   color: #D3D4D4 !important;
+  .align-right-small {
+    @media only screen and (min-width: 640px) {
+      text-align: right;
+    }
+  }
 }
 
 // #nav {
